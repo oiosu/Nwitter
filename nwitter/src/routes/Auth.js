864c28586fabc5e00826140586343e6040ba08fc
oiosu <kdlatnrud123@naver.com>
2023-01-13 16:40:40 +0900

    Creating Account

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     // onChange function 
     const onChange = (event) => {
         // 간단하게 form을 컨트롤 할 수 있는 방법 
@@ -16,8 +18,23 @@ const Auth = () => {
         }
     };
     // onSubmit _ form 제출에 대해서 preventDefault를 하도록 하기
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();
+        try {
+            let data;
+            if(newAccount) {
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email, password
+                );
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -39,7 +56,7 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log in"} />
             </form>
             <div>
                 <button>Continue with Google</button>
